6a57019afbc9bde55db772ed4de0f01a77709399
Pablo-Aguiar <pma765@hotmail.com>
2023-04-25 09:29:28 -0300

    api key update

--- src/app/interceptors/http-headers.interceptor.ts
+++ src/app/interceptors/http-headers.interceptor.ts
@@ -1,25 +1,29 @@
-import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from "@angular/common/http";
-import { Injectable } from "@angular/core";
-import { Observable } from "rxjs";
+import {
+  HttpEvent,
+  HttpHandler,
+  HttpInterceptor,
+  HttpRequest,
+} from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable()
-
 export class HttpHeadersInterceptor implements HttpInterceptor {
-    constructor () {}
+  constructor() {}
 
-    intercept(
-        req: HttpRequest<any>,
-        next: HttpHandler
-    ): Observable<HttpEvent<any>> {
-        req = req.clone({
-            setHeaders: {
-                'x-rapidapi-key': 'esGbwrm390mshS2BCl0RALxQRtZTp1W7sFMjsnyJlJzDXVkW0H',
-                'x-rapidapi-host': 'rawg-video-games-database.p.rapidapi.com',
-            },
-            setParams: {
-                key: 'e40e743af2c94b0c916a8aa618fb4473'
-            }
-        });
-        return next.handle(req);        
-    }
+  intercept(
+    req: HttpRequest<any>,
+    next: HttpHandler
+  ): Observable<HttpEvent<any>> {
+    req = req.clone({
+      setHeaders: {
+        'x-rapidapi-key': 'esGbwrm390mshS2BCl0RALxQRtZTp1W7sFMjsnyJlJzDXVkW0H',
+        'x-rapidapi-host': 'rawg-video-games-database.p.rapidapi.com',
+      },
+      setParams: {
+        key: '53e251c017b04deb9d66e6934936878e',
+      },
+    });
+    return next.handle(req);
+  }
 }
